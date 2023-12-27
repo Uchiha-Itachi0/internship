15f9223acc79efcf7d884412fe37097971e2e934
Anubhav Shukla <86482181+Uchiha-Itachi0@users.noreply.github.com>
2023-12-27 23:03:08 +0530

    Update InternshipApplication.java

--- src/main/java/com/example/internship/InternshipApplication.java
+++ src/main/java/com/example/internship/InternshipApplication.java
@@ -4,10 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class InternshipApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InternshipApplication.class, args);
     }
+    @Bean
+	public WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
 
 }
